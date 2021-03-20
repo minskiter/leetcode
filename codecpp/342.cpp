fba10d2f94773fce8847596ae2b56dbb157a2fb9
minskiter <minskiter@qq.com>
2021-03-20 11:42:57 +0800

    [update] 2021-3-20 总3题

---
+++ codecpp/342.cpp
@@ -0,0 +1,45 @@
+#include "header.h"
+
+#define rep(i, f, t, s) for (int i = f; i <= (int)t; i += s)
+#define repd(i, f, t, s) for (int i = f; i >= (int)t; i -= s)
+#define mk(a, b) make_pair(a, b)
+#define sqr(a) ((a) * (a))
+template <class Ty1, class Ty2>
+inline const pair<Ty1, Ty2> operator+(const pair<Ty1, Ty2> &p1, const pair<Ty1, Ty2> &p2)
+{
+    return mk(p1.first + p2.first, p1.second + p2.second);
+}
+template <class Ty1, class Ty2>
+inline const pair<Ty1, Ty2> operator-(const pair<Ty1, Ty2> &p1, const pair<Ty1, Ty2> &p2)
+{
+    return mk(p1.first - p2.first, p1.second - p2.second);
+}
+template <class Ty1, class Ty2>
+bool inner(pair<Ty1, Ty2> pos, pair<Ty1, Ty2> leftTop, pair<Ty1, Ty2> rightBottom)
+{
+    if (pos.first >= leftTop.first && pos.second >= leftTop.second)
+    {
+        if (pos.first < rightBottom.first && pos.second < rightBottom.second)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+// Solution Here
+class Solution
+{
+public:
+    bool isPowerOfFour(int n)
+    {
+        int bit = 0b1010101010101010101010101010101 & n;
+        return n>0 && n-bit==0 &&  bit > 0 && bit - (bit & -bit) == 0;
+    }
+};
+
+int main()
+{
+    cout << Solution().isPowerOfFour(20) << endl;
+    return 0;
+}
---
+++ codecpp/343.cpp
@@ -0,0 +1,52 @@
+#include "header.h"
+
+#define rep(i, f, t, s) for (int i = f; i <= (int)t; i += s)
+#define repd(i, f, t, s) for (int i = f; i >= (int)t; i -= s)
+#define mk(a, b) make_pair(a, b)
+#define sqr(a) ((a) * (a))
+template <class Ty1, class Ty2>
+inline const pair<Ty1, Ty2> operator+(const pair<Ty1, Ty2> &p1, const pair<Ty1, Ty2> &p2)
+{
+    return mk(p1.first + p2.first, p1.second + p2.second);
+}
+template <class Ty1, class Ty2>
+inline const pair<Ty1, Ty2> operator-(const pair<Ty1, Ty2> &p1, const pair<Ty1, Ty2> &p2)
+{
+    return mk(p1.first - p2.first, p1.second - p2.second);
+}
+template <class Ty1, class Ty2>
+bool inner(pair<Ty1, Ty2> pos, pair<Ty1, Ty2> leftTop, pair<Ty1, Ty2> rightBottom)
+{
+    if (pos.first >= leftTop.first && pos.second >= leftTop.second)
+    {
+        if (pos.first < rightBottom.first && pos.second < rightBottom.second)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+// Solution Here
+class Solution
+{
+public:
+    static vector<int> dp;
+    static int lastn;
+    int integerBreak(int n)
+    {
+        for (int i = lastn; i <= n; ++i)
+            for (int j = 1; j < i; ++j)
+                dp[i] = max(max(dp[i], dp[j] * (i - j)), j * (i - j));
+        return dp[lastn = n];
+    }
+};
+
+vector<int> Solution::dp(59, 1);
+int Solution::lastn = 3;
+
+int main()
+{
+    cout << Solution().integerBreak(58);
+    return 0;
+}
